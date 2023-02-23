7a9a8ea67ebd965c1468ed8a687e0e080ad7d7b7
Paulo Santos <paulo.santos@loviz.local>
2023-02-23 16:42:09 -0300

    refactor: prepare for class

--- application/app.js
+++ application/app.js
@@ -1,29 +1,29 @@
 /// Arrow Function
 
 function soma (a, b){
-  console.log(a + b)
+  return (a + b)
 }
 
-console.log('resultado soma função declarada fora de variável: ',soma(1,2))
-const somaConst = function (a, b){
-  console.log(a + b)
-}
-console.log('resultado soma função dentro da variável: ', somaConst(1,2))
+// console.log('resultado soma função declarada fora de variável: ',soma(1,2))
+// const somaConst = function (a, b){
+//   return(a + b)
+// }
+// console.log('resultado soma função dentro da variável: ', somaConst(1,2))
 // (a, b) => {
 //   console.log(a + b)
 // } *** caso de callback
-const somaArrow = (a, b) => {
-  console.log(a + b)
-}
-console.log('resultado soma arrow function salva dentro de variável: ',somaArrow(1,2))
-const printNum = a =>{
-  console.log(a)
-}
-console.log('console.log de arrow function com um unico parametro: ', printNum(20))
+// const somaArrow = (a, b) => {
+//   return(a + b)
+// }
+// console.log('resultado soma arrow function salva dentro de variável: ',somaArrow(1,2))
+// const printNum = a =>{
+//   return(a)
+// }
+// console.log('console.log de arrow function com um unico parametro: ', printNum(20))
 
-const printNumResumida = a => console.log(a)
-console.log('console.log de arrow function que apenas tem uma linha: ', printNumResumida(20))
+// const printNumResumida = a => a
+// console.log('console.log de arrow function que apenas tem uma linha: ', printNumResumida(20))
 
-const soma2 = (a, b) => a + b
-const resultado = soma2(2, 3)
-console.log('Return omitido em arrow functions quando função apenas tem uma linha: ', resultado)
+// const soma2 = (a, b) => a + b
+// const resultado = soma2(2, 3)
+// console.log('Return omitido em arrow functions quando função apenas tem uma linha: ', resultado)
