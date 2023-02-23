b3e36d06a892bf77a0031851ee521ae0f8fe1fa7
Paulo Santos <paulo.santos@loviz.local>
2023-02-23 16:32:20 -0300

    refactor: prepare for class

--- application/app.js
+++ application/app.js
@@ -10,21 +10,21 @@ let nomeLet = 'novoNome'
 
 var nomeVar = 'novoNome'
 
-if( nomeConst ){
-  const nomeConst2 = 'const'
-  let nomeLet2 = 'let'
-  var nomeVar2 = 'var'
-  console.log(`${nomeConst2} ${nomeLet2} ${nomeVar2}`)
-}
-console.log(nomeConst)
+// if( nomeConst ){
+//   const nomeConst2 = 'const'
+//   let nomeLet2 = 'let'
+//   var nomeVar2 = 'var'
+//   console.log(`${nomeConst2} ${nomeLet2} ${nomeVar2}`)
+// }
+// console.log(nomeConst)
 
-console.log(nomeLet)
+// console.log(nomeLet)
 
-console.log(nomeVar)
+// console.log(nomeVar)
 
 
-console.log(nomeVar2)
+// console.log(nomeVar2)
 
-console.log(nomeLet2)
+// console.log(nomeLet2)
 
-console.log(nomeConst2)
+// console.log(nomeConst2)
