3ded58b9c0c2d28b6f360db81874e3cfba7af3d2
Paulo Santos <paulo.santos@loviz.local>
2023-02-23 16:24:48 -0300

    refactor: const example added

--- application/app.js
+++ application/app.js
@@ -1,31 +1,9 @@
 //Constante
+var nomeVar = 'varname'
+const nomeConst = 'constname'
 
-const nomeConst = true
-
-//Let
-
-let nomeLet = 'novoNome'
-
-//var
-
-var nomeVar = 'novoNome'
-
-if( nomeConst ){
-  const nomeConst2 = 'const'
-  let nomeLet2 = 'let'
-  var nomeVar2 = 'var'
-  console.log(`${nomeConst2} ${nomeLet2} ${nomeVar2}`)
-}
 console.log(nomeConst)
-
-console.log(nomeLet)
-
 console.log(nomeVar)
-
-
-console.log(nomeVar2)
-
-console.log(nomeLet2)
-
-console.log(nomeConst2)
-
+nomeVar = 'nomeVar'
+console.log(nomeVar)
+nomeConst = 'nomeConst'
