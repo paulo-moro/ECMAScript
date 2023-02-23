6bea66866f21d9b1c44d3c63aefcc9f2f93064a3
Paulo Santos <paulo.santos@loviz.local>
2023-02-23 16:34:11 -0300

    refactor: prepare for class

--- application/app.js
+++ application/app.js
@@ -8,19 +8,19 @@ function subtração(a, b){
   console.log(a - b)
 }
 
-function soma(a, b = 0){
-  console.log(a + b)
-}
+// function soma(a, b = 0){
+//   console.log(a + b)
+// }
 
-function subtração(a, b = 0){
-  console.log(a - b)
-}
+// function subtração(a, b = 0){
+//   console.log(a - b)
+// }
 
-function subtraçãoInv(a,b, inverter = false){
-  if(inverter){
-    console.log(b - a)
-  } else {
-    console.log(a - b)
-  }
-}
+// function subtraçãoInv(a,b, inverter = false){
+//   if(inverter){
+//     console.log(b - a)
+//   } else {
+//     console.log(a - b)
+//   }
+// }
 
