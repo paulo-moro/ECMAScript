f93c59af715e20ad1c0fb751ed018b157a08a06e
Paulo Santos <paulo.santos@loviz.local>
2023-02-23 16:35:11 -0300

    refactor: prepare for class

--- application/app.js
+++ application/app.js
@@ -8,19 +8,19 @@ const specialization = 'Arcano'
 
 const race = 'Humano'
 
-const player = {
-  nome: nome,
-  classe: classe,
-  specialization: specialization,
-  race: race
-}
-console.log('Jogador criado da forma convencional: ', player)
+// const player = {
+//   nome: nome,
+//   classe: classe,
+//   specialization: specialization,
+//   race: race
+// }
+// console.log('Jogador criado da forma convencional: ', player)
 
-const newPlayer = {
-  nome,
-  classe,
-  specialization,
-  race
-}
+// const newPlayer = {
+//   nome,
+//   classe,
+//   specialization,
+//   race
+// }
 
-console.log('Jogador criado com JSON encurtado: ', newPlayer)
+// console.log('Jogador criado com JSON encurtado: ', newPlayer)
